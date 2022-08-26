15c30fcc2c4e1e8d9003037641cbb2ee1b33fbea
shinsuke-kuwahara <shinsuke_kuwahara@v-kki.co.jp>
2022-08-26 14:35:37 +0900

    名称２追加

--- kki_product/__manifest__.py
+++ kki_product/__manifest__.py
@@ -20,13 +20,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'product'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        'views/product.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ kki_product/models/product.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class kki_product(models.Model):
+    _inherit = "product.template"
+
+    name2 = fields.Char()
+    marge_name = fields.Char(compute="_get_marge_name", store=True)
+
+    @api.depends('name', 'name2')
+    def _get_marge_name(self):
+        for record in self:
+            if record.name2:
+                record.marge_name = record.name + record.name2
+            else:
+                record.marge_name = record.name
